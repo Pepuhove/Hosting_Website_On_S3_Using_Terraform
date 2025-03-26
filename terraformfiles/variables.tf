1ed7e6aedd00ca114b9187012c4388b54d2f1c3e
Pepukai Hove <118389384+Pepuhove@users.noreply.github.com>
2025-03-26 23:42:57 +0200

    variables.tf

---
+++ terraformfiles/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+    description = "Aws Region"
+    type = string
+    default = "us-east-1"
+  
+}
+
+variable "bucket_name" {
+    description = "Name of the bucket"
+    type = string
+    bucket = "pepu1214"
+  
+}
